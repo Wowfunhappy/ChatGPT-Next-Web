d55affd48f55ee13574eb85ade95950650f3b6bf
Jonathan Alland <Wowfunhappy@gmail.com>
2024-09-02 16:17:30 -0400

    oops

--- build-osx.sh
+++ build-osx.sh
@@ -3,9 +3,9 @@
 set -e
 cd "$(dirname 0)"
 
-#docked nextchat yarn export
-#rm -rf electron-wrapper/src/site
-#cp -r out electron-wrapper/src/site
+docked nextchat yarn export
+rm -rf electron-wrapper/src/site
+cp -r out electron-wrapper/src/site
 npm --prefix electron-wrapper run package-mac
 cp electron-wrapper/robot.icns electron-wrapper/release/*-darwin-x64/*.app/Contents/Resources/electron.icns
 ./electron-wrapper/release/*-darwin-x64/*.app/Contents/MacOS/*
